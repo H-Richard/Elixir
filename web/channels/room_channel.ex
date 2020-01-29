cfb08b8d989283fab088e5f3870138fdf92b7158
H-Richard <r.hong@pm.me>
2020-01-29 15:42:55 -0500

    finished base

--- web/channels/room_channel.ex
+++ web/channels/room_channel.ex
@@ -15,4 +15,13 @@ defmodule Chat.RoomChannel do
         push socket, "presence_state", Presence.list(socket)
         {:noreply, socket}
     end
+
+    def handle_in("message:new", message, socket) do
+        broadcast! socket, "message:new", %{
+            user: socket.assigns.user,
+            body: message,
+            timestamp: :os.system_time(:milli_seconds)
+        }
+        {:noreply, socket}
+    end
 end
--- web/static/js/app.js
+++ web/static/js/app.js
@@ -19,3 +19,79 @@ import "phoenix_html"
 // paths "./socket" or full ones "web/static/js/socket".
 
 // import socket from "./socket"
+
+import {Socket, Presence} from "phoenix"
+
+let user = document.getElementById("user").innerText
+console.log("user", user)
+let socket = new Socket("/socket", {params: {user: user}})
+socket.connect()
+
+let presences = {}
+
+let formatedTimestamp = (ts) => {
+    let date = new Date(ts)
+    return date.toLocaleTimeString()
+}
+
+let listBy = (user, {metas: metas}) => {
+    return {
+        user: user,
+        onlineAt: formatedTimestamp(metas[0].online_at)
+    }
+}
+
+let userList = document.getElementById("userList")
+
+let render = (presences) => {
+    userList.innerHTML = Presence.list(presences, listBy)
+        .map(presence => 
+            `
+            <li>
+                ${presence.user}
+                <br>
+                <small>Online Since: ${presence.onlineAt}</small>
+            </li>
+            `
+        ).join("")
+}
+
+let room = socket.channel("room:lobby")
+room.on("presence_date", state => {
+    presences = Presence.syncState(presences, state)
+    render(presences)
+})
+
+room.on("presence_diff", diff => {
+    presences = Presence.syncDiff(presences, diff)
+    render(presences)
+})
+
+room.join()
+
+let messageInput = document.getElementById("newMessage")
+
+console.log(messageInput)
+messageInput.addEventListener("keypress", (e) => {
+    if (e.keyCode == 13 && messageInput.value != "") {
+        console.log("New message:", messageInput.value)
+        room.push("message:new", messageInput.value)
+        messageInput.value = ""
+    }
+})
+
+let messageList = document.getElementById("messageList")
+console.log(messageList)
+let renderMessage = (message) => {
+    let messageElement = document.createElement('li')
+    messageElement.innerHTML = `
+        <b>${message.user}</b>
+        <i>${formatedTimestamp(message.timestamp)}</i>
+        <p>${message.body}</p>
+        `
+    messageList.appendChild(messageElement)
+    messageList.scrollTop = messageList.scrollHeight
+}
+
+room.on("message:new", message => renderMessage(message))
+
